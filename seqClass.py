5037609569403c1c7a4eebcdb04b709abc4247bf
jamillabeek <jamillabeek@icloud.com>
2026-03-13 11:37:41 +0100

    added percentages of nucleotides as output message and prevent sequence with both U and T to be classified as the wrong type

--- seqClass.py
+++ seqClass.py
@@ -4,7 +4,9 @@ import sys, re
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+# add the s argument to input a sequence, make it required and specify that it should be a string
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+# add the -m argument to search a motif in the sequence
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
@@ -15,12 +17,16 @@ args = parser.parse_args()
 args.seq = args.seq.upper()
 
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+    if re.search('T', args.seq) and not re.search('U', args.seq): # if it contains T and U the sequence is wrong
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq) and not re.search('T', args.seq): # if it contains U and T the sequence is wrong
         print ('The sequence is RNA')
     else:
         print ('The sequence can be DNA or RNA')
+
+    # compute the percentage of each nucleotide in the sequence
+    for nt in 'ACGTU':
+        print(f'{nt}: {args.seq.count(nt) / len(args.seq) * 100:.2f}%')
 else:
     print ('The sequence is not DNA nor RNA')
 
